b3ed4233a09dbd9f2dc22f1e6b617ee4ff88b2c5
stuartmorgan-g <stuartmorgan@google.com>
2026-05-28 07:29:46 -0400

    [vector_math] Fix old license headers (#6)
    A couple of files had old `(c) Google` license header blocks, so failed
    the standardized license block check when I tried to turn it on. This
    fixes them.
    
    Enforcement will be a separate PR that turns on a bunch of repo tooling
    checks.
    
    *Replace this paragraph with a description of what this PR is changing
    or adding, and why. Consider including before/after screenshots.*
    
    *List which issues are fixed by this PR. You must list at least one
    issue.*
    
    *If you had to change anything in the [flutter/tests] repo, include a
    link to the migration guide as per the [breaking change policy].*
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read the [Flutter Style Guide] _recently_, and have followed its
    advice.
    - [x] I signed the [CLA].
    - [ ] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] All existing and new tests are passing.
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Style-guide-for-Flutter-repo.md
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Chat.md

--- packages/vector_math/benchmark/matrix4_tween_bench.dart
+++ packages/vector_math/benchmark/matrix4_tween_bench.dart
@@ -1,6 +1,6 @@
-// Copyright (c) 2019, Google Inc. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:vector_math/vector_math_64.dart';
